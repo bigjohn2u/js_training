7d1c162aab5ba762a7cb867933ab452baa2936cd
bigjohn2u <https://github.com/bigjohn2u/js_training.git>
2017-11-05 16:21:11 -0800

    initial load

---
+++ test10.js
@@ -0,0 +1,17 @@
+var food = "fast food";
+if (food == "apple") {
+	alert (" we ate and apple");
+
+}
+
+else if (food == "meat") {
+	alert (" we ate some meat");
+}	
+
+else if (food == "fast food") {
+	alert (" we ate some fast food");
+}
+
+else {
+	alert (" we ate something else");
+}
